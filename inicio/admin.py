8b0a5c8fb04f11ce1e75bcba9265585917c27d28
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-06-27 20:22:43 -0300

    Playground Intermedio II

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from inicio.models import Perro
+
+admin.site.register(Perro)
 
 # Register your models here.
--- inicio/urls.py
+++ inicio/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from inicio import views
 
+app_name = 'inicio'
+
 urlpatterns = [
-    path('', views.inicio),
-    path('prueba/', views.prueba),
+    path('', views.inicio, name='inicio'),
+    path('prueba/', views.prueba, name='prueba'),
     path('segunda-vista', views.segunda_vista),
     path('crear-perro/<str:nombre>/<int:edad>/', views.crear_perro)
 ]
